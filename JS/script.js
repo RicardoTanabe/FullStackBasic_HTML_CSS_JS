0575b46ab31a081595e9b4fa6bbd8fae01f71844
Ricardo Kenji Ramos Tanabe <ricardouhtanabe@hotmail.com>
2024-11-10 20:32:40 -0300

    Alterações no layout da página

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,23 @@ document.getElementById("header").innerHTML = fetch('MODEL/header.html')
 document.getElementById("footer").innerHTML = fetch('MODEL/footer.html')
     .then(response => response.text())
     .then(data => document.getElementById("footer").innerHTML = data);
+
+const menuList = ["cachorro", "gato", "passaro", "peixe",
+    "outrospets", "casaejardim", "promocoes", "servicos", "clubes"]
+
+const menuListDict = {
+    "cachorro": ["racao", "petiscoseossos", "tapetesfraldasebanheiros",
+        "farmacia"],
+    "gato": [],
+    "passaro": [],
+    "peixe": [],
+    "outrospets": [],
+    "casaejardim": [],
+    "promocoes": [],
+    "servicos": [],
+    "clubes": [],
+    "racao": ["racaoseca", "racaoumida", "vertodos"],
+    "petiscoseossos": ["cuidadooral", "petiscosnaturais", "vertodos"],
+    "tapetesfraldasebanheiros": ["tapeteshigienicos", "fraldas", "vertodos"],
+    "farmacia": ["antipulgasecarrapatos", "demaismedicamentos", "vertodos"],
+}
