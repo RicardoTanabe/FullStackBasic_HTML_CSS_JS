2ff5e0beb09811883ce770004924401310b37502
RicardoTanabe <ricardo.tanabe@discente.ufma.br>
2024-11-12 16:55:28 -0300

    Adição do form em Cadastre-se; img Petshop link home

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,26 @@
 document.getElementById("header").innerHTML = fetch('MODEL/header.html')
     .then(response => response.text())
-    .then(data => document.getElementById("header").innerHTML = data);
+    .then(data => {
+        document.getElementById("header").innerHTML = data
+
+        const menuItems = document.querySelectorAll("#header-bottom-menu-layer-1 > li");
+        const main = document.querySelector("main");
+
+        menuItems.forEach((item, index) => {
+            if (index < menuItems.length - 3) {
+                item.addEventListener("mouseenter", () => {
+                    main.classList.add("overlay-active");
+                });
+
+                item.addEventListener("mouseleave", () => {
+                    main.classList.remove("overlay-active");
+                });
+            }
+        })
+    });
 
 document.getElementById("footer").innerHTML = fetch('MODEL/footer.html')
     .then(response => response.text())
-    .then(data => document.getElementById("footer").innerHTML = data);
-
-const menu = document.querySelector("main");
-const main = document.querySelector("#main");
-
-menu.addEventListener("mouseenter", () => {
-    console.log("Overlay ativado");
-    main.classList.add("overlay-active");
-});
-
-menu.addEventListener("mouseleave", () => {
-    console.log("Overlay desativado");
-    main.classList.remove("overlay-active");
-});
+    .then(data => {
+        document.getElementById("footer").innerHTML = data
+    });
